7c44a0510c62eca4d83575983fbf76915f7399fb
Eren <90460505+erenuysaldev@users.noreply.github.com>
2025-04-22 11:07:06 +0300

    Dizilere geçildi

---
+++ 11.gün/Diziler/Diziler/Diziler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region not ortalaması
+            int[] notlar = { 70, 85, 90, 65, 100 };
+            int toplam = 0;
+
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplam += notlar[i];
+            }
+
+            double ortalama = (double)toplam / notlar.Length;
+            Console.WriteLine($"Ortalama: {ortalama}");
+            #endregion
+        }
+    }
+}
