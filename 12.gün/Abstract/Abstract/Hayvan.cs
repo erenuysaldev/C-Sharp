2fba6efbaa4d9b9db8b8f9c51a667ec0f42911cb
Eren <90460505+erenuysaldev@users.noreply.github.com>
2025-04-25 10:48:11 +0300

    Abstract

---
+++ 12.gün/Abstract/Abstract/Hayvan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Abstract Class ve Interface Arasındaki Farklar:
+Özellik | Abstract Class | Interface
+Metotlar | Hem soyut (abstract) hem de somut olabilir. | Sadece soyut metotlar içerir.
+Kalıtım | Sadece tek sınıfı miras alabilir. | Birden fazla interface implement edilebilir.
+Üyeler | Alanlar, constructorlar ve methodlar olabilir. | Yalnızca metot imzaları ve özellikler olabilir.
+Amacı | Ortak davranışları tanımlar ve alt sınıflara sağlar. | Sadece bir "sözleşme" tanımlar.
+
+
+*/
+namespace Abstract
+{
+    public abstract class Hayvan
+    {
+        public string Ad { get; set; }
+
+        // Soyut metot
+        public abstract void SesCikar();  // Alt sınıflar bu methodu implement etmeli.
+
+        // Normal metot (Alt sınıflar değiştirebilir veya kullanabilir)
+        public void Yuru()
+        {
+            Console.WriteLine($"{Ad} yürüdü.");
+        }
+    }
+    public class Kopek : Hayvan
+    {
+        public override void SesCikar()
+        {
+            Console.WriteLine($"{Ad} havladı! ");
+        }
+    }
+
+    public class Kedi : Hayvan
+    {
+        public override void SesCikar()
+        {
+            Console.WriteLine($"{Ad} miyavladı! ");
+        }
+    }
+}
---
+++ 12.gün/Abstract/Abstract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hayvan h1 = new Kopek { Ad = "Karabaş" };
+            Hayvan h2 = new Kedi { Ad = "Tekir" };
+
+            h1.SesCikar();  // Kopek havladı! 
+            h2.SesCikar();  // Kedi miyavladı! 
+
+            h1.Yuru();      // Kopek yürüdü.
+            h2.Yuru();      // Kedi yürüdü.
+        }
+    }
+}
